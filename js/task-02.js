7def2879ac51b1f4db52db6cbecc940c7f38194f
YurijKovbasyuk <93015127+YurijKovbasyuk@users.noreply.github.com>
2022-04-25 23:21:17 +0300

    task 02-03 (#1)
    Добавлені зміни

--- js/task-02.js
+++ js/task-02.js
@@ -7,13 +7,20 @@ const ingredients = [
   'Condiments',
 ];
 
-
-const list = ingredients.map((ingredient) => `<li class="item">${ingredient}</li>`).join("");
-
+// const list = ingredients.map((ingredient) => `document.createElment(<li class="item">${ingredient}</li>)`).join("");
 const item = document.querySelector('#ingredients')
+// item.innerHTML += list
 
-item.innerHTML += list
 
+let listIngredients = '';
+const createForm = () => {
+  for (const ingredient of ingredients) {
+    const li = document.createElement("li");
+    li.textContent = ingredient;
+    li.classList = 'item';
+    listIngredients = listIngredients + li.outerHTML;
+  }
+}
+createForm();
 
-console.log(item);
-
+item.insertAdjacentHTML("afterbegin", listIngredients)

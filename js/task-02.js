ef34502fd4fd2201408d101cbd66f9906f2fbf90
YurijKovbasyuk <kovbasyuk.yurij@gmail.com>
2022-04-27 13:19:16 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,21 +6,37 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// ====1==
 
 // const list = ingredients.map((ingredient) => `document.createElment(<li class="item">${ingredient}</li>)`).join("");
-const item = document.querySelector('#ingredients')
+// const item = document.querySelector('#ingredients')
 // item.innerHTML += list
 
+// ====2==
+// const item = document.querySelector('#ingredients')
 
-let listIngredients = '';
-const createForm = () => {
-  for (const ingredient of ingredients) {
-    const li = document.createElement("li");
-    li.textContent = ingredient;
-    li.classList = 'item';
-    listIngredients = listIngredients + li.outerHTML;
-  }
-}
-createForm();
+// let listIngredients = '';
+// const createForm = () => {
+//   for (const ingredient of ingredients) {
+//     const li = document.createElement("li");
+//     li.textContent = ingredient;
+//     li.classList = 'item';
+//     listIngredients = listIngredients + li.outerHTML;
+//   }
+// }
+// createForm();
 
-item.insertAdjacentHTML("afterbegin", listIngredients)
+// item.insertAdjacentHTML("afterbegin", listIngredients)
+
+// ====3==
+const listIngredients = document.querySelector('#ingredients');
+
+const markup = (ingredient) => {
+  const element = document.createElement('li');
+  element.textContent = ingredient;
+  return element;
+};
+console.dir(markup);
+const createMarkup = ingredients.map(markup);
+console.dir(createMarkup);
+listIngredients.append(...createMarkup);
